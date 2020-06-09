9ef43b4b9475eeec234e213ac5f2eee8f64df950
Diego Moraes <dmoraes@prometeoapi.com>
2020-06-08 21:10:48 -0300

    fix

--- logic/client.py
+++ logic/client.py
@@ -22,20 +22,19 @@ class Client:
     async def fetch(self, session, base_url):
         try:
             async with session.get(base_url, timeout=3) as response:
-                REQUESTS = REQUESTS + 1
-                print(REQUESTS)
+
                 if response.status == 200:
                     print("SUCCESS::{0}".format(base_url))
                     elapsed = default_timer() - START_TIME
                     time_completed_at = "{:5.2f}s".format(elapsed)
                     print("{0:<30} {1:>20}".format(base_url, time_completed_at))
-                delay = response.headers.get("DELAY")
-                date = response.headers.get("DATE")
-                print("{}:{} with delay {}".format(date, response.url, delay))
+                #delay = response.headers.get("DELAY")
+                #date = response.headers.get("DATE")
+                #print("{}:{} with delay {}".format(date, response.url, delay))
                 return await response.text()
         except UnicodeError:
             print('unicode error')
-        except:
+        except Exception as e:
             pass
 
     async def bound_fetch(self, sem, session, url):
@@ -52,65 +51,3 @@ class Client:
 
             responses = asyncio.gather(*tasks)
             await responses
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-    async def get_tasks(self, urls, words, workers):
-        async with ClientSession() as session:
-            tasks = []
-            START_TIME = default_timer()
-            for base_url in urls[:words]:
-                task = asyncio.ensure_future(self.bound_fetch(sem, session, base_url))
-                tasks.append(task)
-            return tasks
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-                #data = response.text
-                #if response.status_code == 200:
-                #    print("SUCCESS::{0}".format(base_url))
-                #    elapsed = default_timer() - START_TIME
-                #    time_completed_at = "{:5.2f}s".format(elapsed)
-                #    print("{0:<30} {1:>20}".format(base_url, time_completed_at))
-                #
-                #return {
-                #    'status': response.status_code,
-                #    'url': response.url
-                #}
-
-        #except Exception as ex:
-        #    logging.warning(ex)
